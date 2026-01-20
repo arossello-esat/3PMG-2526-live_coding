3b39a14b15a01dc7a02c0b086ceafa5423a96a16
Arnau Rosselló <arossello@esat.es>
2026-01-20 20:26:40 +0100

    More scripting

--- scripting.cc
+++ scripting.cc
@@ -26,6 +26,44 @@ struct ECSManager {
 
 struct LifeComponent { float value;};
 
+struct ScriptComponent {
+    ScriptComponent() {
+        luastate_.open_libraries(sol::lib::base);
+        LoadState();
+    }
+
+
+    ScriptComponent(const std::string& code) {
+        luastate_.open_libraries(sol::lib::base);
+        LoadState();
+        sol::load_result chunk = state.load(lua_program);
+        if(chunk.valid()) chunk();
+    }
+
+    LoadCppDefinitions() {
+        state.usertype<ECSManager> lua_ecs = state.new_usertype<ECSManager>("ECS",sol::no_constructor);
+        state.new_usertype<LifeComponent>("Life",
+            sol::no_constructor,
+            "value", &LifeComponent::value);
+        std::reference_wrapper<ECSManager> recs = std::ref(ecs);
+        state.set("ECS",recs);
+    }
+    sol::state luastate_;
+};
+
+void f1(auto v);
+
+template<typename T> void f2(T v);
+
+UpdateLuaSystem(std::vector<ScriptComponent>& scv) {
+    for(auto component : scv) {
+        sol::protected_function on_frame = componente.state["on_frame"];
+        if(on_frame.valid()) {
+            sol::protected_function_result  res= on_frame();
+        }
+    }
+    for() // ELIMINAR OBJETOS QUE ALGUN SCRIPT HAYA MARCADO PARA DESTRUCCION
+}
 int main(int,char**) {
 
     sol::state state;
@@ -40,13 +78,34 @@ int main(int,char**) {
         ecs->*ptm();
     }
 
-    state.usertype<ECSManager> lua_ecs = state.new_usertype<ECSManager>("ECS");
+    state.usertype<ECSManager> lua_ecs = state.new_usertype<ECSManager>("ECS",sol::no_constructor);
+
+
+    auto get_index = [](Context& self,const std::string& key, sol::object value) -> sol::object {
+        sol::object result = current_[key];
+        if(result.valid()) return result;
+        return sol::nil;
+    }
+
+    auto get_new_index = [](Context& self,const std::string& key, sol::object value) {
+        current_[key] = value;
+    }
+
+    sol::object* current;
+    state["ctx"] = current;
+
+    auto lua_ecs = state.new_usertype<LuaContext>("Context",
+        sol::no_constructor,
+        sol::meta_function::index, get_index,
+        sol::meta_function::new_index, get_new_index
+    );
 
     for(auto&& [key,value] : type_map) {
             lua_ecs["Get"+ value.name()] = value.GetComponent;
     }
     
     state.new_usertype<LifeComponent>("Life",
+        sol::no_constructor,
         "value", &LifeComponent::value
     );
 
@@ -57,9 +116,14 @@ int main(int,char**) {
     if(chunk.valid()) {
         chunk();
 
-        sol::protected_function add_life = state["add_life"];
-        state["shield"];
-        sol::protected_function_result  res= add_life(player);
+
+        auto shield = state["shield"];
+        if(shield.valid()) 
+        for(auto componente = componenteList.begin()...) {
+            sol::protected_function on_frame = componente.state["on_frame"];
+            if(add_life.valid())
+               sol::protected_function_result  res= on_frame();
+        }
         bool full_life = res;
     }
 
